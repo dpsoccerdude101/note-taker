fad9ad09a426c350e1394c64b1cbc6a74dcf4cd2
dpsoccerdude101 <dennispavlyuk@gmail.com>
2020-11-10 11:44:07 -0500

    added dave's spicy changes

---
+++ animation.js
@@ -0,0 +1,29 @@
+var KEYFRAMES = {
+    o: [
+      {opacity: 1},
+      {opacity: 0},
+      {opacity: 1}
+    ],
+    t: [
+      {transform: 'translateX(0)'},
+      {transform: 'translateX(200px)'},
+      {transform: 'translateX(0)'}
+    ],
+    r: [
+      {transform: 'rotate(0deg)'},
+      {transform: 'rotate(200deg)'},
+      {transform: 'rotate(0deg)'}
+    ],
+    bb: [
+      {background: '#4598b6'},
+      {background: '#444444'},
+      {background: '#4598b6'}
+    ],
+    br: [
+      {background: '#b64598'},
+      {background: '#444444'},
+      {background: '#b64598'}
+    ]
+  };
+  
+  
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (function () {
   const model = {
     data: {
+      id: "0",
       name: "Dennis Pavlyuk",
       username: "dpsoccerdude101",
       email: "dpavl1@brockport.edu",
@@ -31,9 +32,9 @@
       data.push(obj);
       localStorage.users = JSON.stringify(data);
     },
-    remove: function (obj) {
+    remove: function (userID) {
       let data = JSON.parse(localStorage.users);
-      let tempData = data.filter((user) => user.content != obj);
+      let tempData = data.filter((user) => user.id != userID);
       localStorage.users = JSON.stringify(tempData);
     },
     getAllNotes: function () {
@@ -53,8 +54,8 @@
       view.render();
     },
 
-    deleteNote: function (note) {
-      model.remove(note);
+    deleteNote: function (userID) {
+      model.remove(userID);
       view.render();
     },
 
@@ -111,38 +112,47 @@
     render: function () {
       let htmlStr = ``;
       const usersArr = controller.getUsers();
-
       userMarkup = (userObj) => {
         return Object.keys(userObj).length === 0
           ? ``
-          : `<div class="user" data-id="note-at-${userObj.timestamp}">
-        <h2 class="name">${userObj.name}</h2>
-        <p class="username">${userObj.username}</p>
-        <p class="email"><a href="">${userObj.email}</a></p>
-        <div class="address">
-          <p class="street">${userObj.address.street}</p>
-          <p class="suite">${userObj.address.suite}</p>
-          <p class="city">${userObj.address.city}</p>
-          <p class="zip">${userObj.address.zip}</p>
-          <div class="geo">
-            <p class="lat">${userObj.address.geo.lat}</p>
-            <p class="lng">${userObj.address.geo.lng}</p>
+          : ` <div class="user" data-id="note-at-${userObj.timestamp}" value="${userObj.id}">
+          <div class="picture">
+            <img class="pix" src="generic_profile.svg">
+            </div>
+          
+          <div class="namecontainer">
+            <div>
+              <h2 class="name">${userObj.name}</h2>
+              <p class="username">${userObj.username}</p>
+              <p class="email"><a href="">${userObj.email}</a></p>
+            </div>
           </div>
-        </div>
-        <p class="phone">${userObj.phone}</p>
-        <p class="website">${userObj.website}</p>
-        <div class="company">
-          <p class="name">${userObj.company.name}</p>
-          <p class="catchPhrase">${userObj.company.catchPhrase}</p>
-          <p class="bs">${userObj.company.bs}</p>
-        </div>
-        <span class="user-date">${userObj.timestamp}</span>
+          <div class="address">
+            <div>
+              <div class="line1">
+                <p class="street">${userObj.address.street}</p>
+                <p class="suite">${userObj.address.suite}</p>
+              </div>
+              <div class="line2">
+                <p class="city">${userObj.address.city}</p>
+                <p class="zip">${userObj.address.zip}</p>
+              </div>
+              <p class="phone">${userObj.phone}</p>
+              <p class="website">${userObj.website}</p>
+            </div>
+          </div>
+          <div class="company">
+            <p class="name">${userObj.company.name}</p>
+            <p class="catchPhrase">${userObj.company.catchPhrase}</p>
+            <p class="bs">${userObj.company.bs}</p>
+          </div>
+          <span class="user-date">${userObj.timestamp}</span>
         <button class="delete">Delete</button>
-      </div>`;
+        </div>`;
       };
 
-      const usersViewArr = usersArr.map(function (note) {
-        return this.userMarkup(note);
+      const usersViewArr = usersArr.map(function (user) {
+        return this.userMarkup(user);
       });
       htmlStr = usersViewArr.join("");
       this.usersList.innerHTML = htmlStr;
